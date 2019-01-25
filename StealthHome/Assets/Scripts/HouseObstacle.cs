2257a74c65665f1d2a89ccf83998a235c649c701
40206111 <40206111@live.napier.ac.uk>
2019-01-25 23:46:35 +0000

    Working Animations
    -visible before light comes on

--- StealthHome/Assets/Scripts/HouseObstacle.cs
+++ StealthHome/Assets/Scripts/HouseObstacle.cs
@@ -2,39 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(SpriteRenderer), typeof(Animator))]
 public class HouseObstacle : MonoBehaviour
 {
-    //Sprite renderer
-    private SpriteRenderer sr;
+    private enum State
+    {
+        Off,
+        Inbetween,
+        On
+    }
+
+    //animator
+    private Animator anim;
 
     //Variables to be changed within Unity
     [SerializeField]
-    private Sprite lightsOff;      //sprite with lights off
+    private float minWait = 1.0f;           //minimum wait until light can come on again
     [SerializeField]
-    private Sprite lightsOn;        //sprite with lights on
+    private float maxWait = 10.0f;           //maximum wait until light can come on again
     [SerializeField]
-    float minWait = 1.0f;           //minimum wait until light can come on again
+    private float chance = 0.1f;            //chance of light coming on
     [SerializeField]
-    float maxWait = 10.0f;           //maximum wait until light can come on again
+    private float onWaitMin = 0.5f;         //minimum wait until light turns off
     [SerializeField]
-    float chance = 0.1f;            //chance of light coming on
+    private float onWaitMax = 1.5f;         //maximum wait until light turns off
     [SerializeField]
-    float onWaitMin = 0.5f;         //minimum wait until light turns off
+    private float checkFreq = 1.0f;         //how often to check if light has turned on
     [SerializeField]
-    float onWaitMax = 1.5f;         //maximum wait until light turns off
-    [SerializeField]
-    float checkFreq = 1.0f;         //how often to check if light has turned on
+    private float warning = 1.0f;
 
     //Is light on
-    public bool lightUp = false;
     private float timeElapsed = 0.0f;
     private float randomOnTime = 0.0f;
     private float totalWait = 0.0f;
+    private State stage = State.Off;
+
+    //Get if light is on from different class
+    public bool IsLightOn()
+    {
+        if (stage == State.On)
+        {
+            return true;
+        }
+        return false;
+    }
 
     void Start()
     {
-        sr = gameObject.GetComponent<SpriteRenderer>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -43,17 +58,24 @@ public class HouseObstacle : MonoBehaviour
         timeElapsed += Time.deltaTime;
 
         //Check if light goes on
-        if (!lightUp && timeElapsed > totalWait)
+        if (stage == State.Off && timeElapsed > totalWait)
         {
             StartCoroutine(Light());
         }
-        else if (lightUp)
+        else if (stage == State.Inbetween && timeElapsed >= warning)
+        {
+            timeElapsed = 0.0f;
+            anim.SetInteger("Stage", (int)State.On);
+            stage = State.On;
+
+        }
+        else if (stage == State.On)
         {
             if (timeElapsed >= randomOnTime)
             {
                 timeElapsed = 0.0f;
-                lightUp = false;
-                sr.sprite = lightsOff;
+                stage = State.Off;
+                anim.SetInteger("Stage", (int)State.Off);
                 totalWait = Random.Range(minWait, maxWait);
             }
 
@@ -70,8 +92,8 @@ public class HouseObstacle : MonoBehaviour
         float CheckLight = Random.Range(0.0f, 1.0f);
         if (CheckLight <= chance)
         {
-            sr.sprite = lightsOn;
-            lightUp = true;
+            anim.SetInteger("Stage", (int)State.Inbetween);
+            stage = State.Inbetween;
             timeElapsed = 0.0f;
             randomOnTime = Random.Range(onWaitMin, onWaitMax);
         }
