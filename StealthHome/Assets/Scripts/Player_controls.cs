4f1622463d6edc5fa4d886dd3ef1434244774604
Paulius Bieksa <priverstinis@gmail.com>
2019-01-25 22:11:37 +0000

    Placeholders for player controls + camera follow.

--- StealthHome/Assets/Scripts/Player_controls.cs
+++ StealthHome/Assets/Scripts/Player_controls.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Player_controls : MonoBehaviour
 {
+    [SerializeField]
+    ControlHandler ch;
+
+    const float movement_speed = 2.0f;
+    const float sprint_speed = 3.0f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,24 @@ public class Player_controls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ch.right)
+        {
+            float speed = movement_speed;
+            if (ch.sprint)
+                speed = sprint_speed;
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(speed * Time.deltaTime, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (ch.left)
+        {
+            float speed = movement_speed;
+            if (ch.sprint)
+                speed = sprint_speed;
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed * Time.deltaTime, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (ch.crouch)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+            /////////////// Play animation for coruching;
+        }
     }
 }
