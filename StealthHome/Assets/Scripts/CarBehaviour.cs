20b2751a2e8400272fa1772eab4589f23e8772ee
Dan's Laptop <dkorsah15@aol.co.uk>
2019-01-26 00:10:05 +0000

    Constant moving.

---
+++ StealthHome/Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private AlertBar Alert;
+    [SerializeField] private float maxSpeed = 75;
+    Rigidbody2D rb;
+
+    void Start ()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        rb.velocity = new Vector2 (-maxSpeed * Time.deltaTime, 0);
+
+    }
+}
