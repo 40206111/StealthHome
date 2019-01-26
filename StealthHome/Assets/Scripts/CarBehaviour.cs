c5c391a7aa13888490ea5b0542c1ed300059ec39
Dan's Laptop <dkorsah15@aol.co.uk>
2019-01-26 01:59:24 +0000

    Car comes to a stop smoothly when spotted. make Controls and car into prefabs.

--- StealthHome/Assets/Scripts/CarBehaviour.cs
+++ StealthHome/Assets/Scripts/CarBehaviour.cs
@@ -10,6 +10,8 @@ public class CarBehaviour : MonoBehaviour
     Rigidbody2D rb;
     int dir;
 
+    public bool Spotted = false;
+
     void Start ()
     {
         if (gameObject.transform.position.x - Camera.main.transform.position.x > 0)
@@ -22,7 +24,35 @@ public class CarBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-        rb.velocity = new Vector2 (dir * maxSpeed * Time.deltaTime, 0);
 
+        //debug
+        if (Input.GetKeyDown ("space"))
+        {
+            Spotted = !Spotted;
+        }
+
+        if (!Spotted)
+        {
+            rb.velocity = new Vector2 (dir * maxSpeed * Time.deltaTime, 0);
+        }
+        else
+        {
+            int dirModifier = dir * -1;
+
+            //slow down
+            if (rb.velocity.x > 0 || rb.velocity.x < 0)
+            {
+                //rb.velocity += new Vector2 (0.1f * dirModifier, 0);
+                rb.velocity = Vector2.Lerp (rb.velocity, new Vector2 (0, 0), 0.01f);
+                Debug.ClearDeveloperConsole ();
+                Debug.Log (rb.velocity.x);
+            }
+
+            if (rb.velocity.x < 0.1f && rb.velocity.x > 0 || rb.velocity.x > -0.1f && rb.velocity.x < 0)
+            {
+                rb.velocity = new Vector2 (0, 0);
+            }
+
+        }
     }
 }
