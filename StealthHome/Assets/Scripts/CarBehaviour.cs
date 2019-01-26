3b7f5173440d8491b85066ff762b2409a14cef45
Dan's Laptop <dkorsah15@aol.co.uk>
2019-01-26 00:46:31 +0000

    Have starting position determine movement direction of car.

--- StealthHome/Assets/Scripts/CarBehaviour.cs
+++ StealthHome/Assets/Scripts/CarBehaviour.cs
@@ -8,16 +8,21 @@ public class CarBehaviour : MonoBehaviour
     [SerializeField] private AlertBar Alert;
     [SerializeField] private float maxSpeed = 75;
     Rigidbody2D rb;
+    int dir;
 
     void Start ()
     {
+        if (gameObject.transform.position.x - Camera.main.transform.position.x > 0)
+            dir = -1;
+        else
+            dir = 1;
         rb = gameObject.GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
     void Update ()
     {
-        rb.velocity = new Vector2 (-maxSpeed * Time.deltaTime, 0);
+        rb.velocity = new Vector2 (dir * maxSpeed * Time.deltaTime, 0);
 
     }
 }
