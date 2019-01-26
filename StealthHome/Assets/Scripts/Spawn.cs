b406b9ad4c8f4dc046a2a52ce9a3f0fd190a68e8
Dan's Laptop <dkorsah15@aol.co.uk>
2019-01-26 13:56:21 +0000

    Start on Spawner

---
+++ StealthHome/Assets/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject Spottable;
+    // Start is called before the first frame update
+    void Start ()
+    {
+
+    }
+
+    void Spawn ()
+    {
+
+        Instantiate (Spottable);
+    }
+
+    IEnumerator timer ()
+    {
+        yield return new WaitForSeconds (5);
+
+    }
+}
