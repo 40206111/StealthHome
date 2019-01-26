34155078a5d6e67a842c1e059b94c71837362b40
40206111 <40206111@live.napier.ac.uk>
2019-01-26 18:31:38 +0000

    Player Animations
    -Player Animates

--- StealthHome/Assets/Scripts/Player_controls.cs
+++ StealthHome/Assets/Scripts/Player_controls.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
 public class Player_controls : MonoBehaviour
 {
     [SerializeField]
@@ -12,17 +13,37 @@ public class Player_controls : MonoBehaviour
     const float movement_speed = 80.0f;
     const float sprint_speed = 130.0f;
 
+    //animator
+    private Animator anim;
+    //Sprite renderer
+    private SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start ()
     {
         player = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void FixedUpdate ()
     {
+        if (ch.MoveRight || ch.MoveLeft)
+        {
+            anim.SetBool("Walk", true);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+
         if (ch.MoveRight)
         {
+            if(sr.flipX)
+            {
+                sr.flipX = false;
+            }
             float speed = movement_speed;
             if (ch.Sprint)
                 speed = sprint_speed;
@@ -30,6 +51,10 @@ public class Player_controls : MonoBehaviour
         }
         if (ch.MoveLeft)
         {
+            if (!sr.flipX)
+            {
+                sr.flipX = true;
+            }
             float speed = movement_speed;
             if (ch.Sprint)
                 speed = sprint_speed;
@@ -41,6 +66,12 @@ public class Player_controls : MonoBehaviour
         {
             player.velocity = new Vector2 (0.0f, 0.0f);
             /////////////// Play animation for coruching;
+            anim.SetBool("Walk", false);
+            anim.SetBool("Crouch", true);
+        }
+        else
+        {
+            anim.SetBool("Crouch", false);
         }
     }
 }
