ce3b6c3d6465c39c6d2cb916abcec3f43243b2bd
Dan's Laptop <dkorsah15@aol.co.uk>
2019-01-27 12:29:30 +0000

    Switching to next level complete.

---
+++ StealthHome/Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour
+{
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+        int nextIndex = currentIndex + 1;
+        SceneManager.LoadScene (nextIndex);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+}
