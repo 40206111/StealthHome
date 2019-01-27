3c259843a02328bb94304b847b690d7f24956073
40206111 <40206111@live.napier.ac.uk>
2019-01-27 14:07:37 +0000

    Ending Animation
    -Timed with music

--- StealthHome/Assets/Scripts/LevelSwitch.cs
+++ StealthHome/Assets/Scripts/LevelSwitch.cs
@@ -7,10 +7,12 @@ public class LevelSwitch : MonoBehaviour
 {
     AudioSource mainAudio;
 
+    private GameObject player;
+
     private void Start ()
     {
         mainAudio = GameObject.FindGameObjectWithTag ("MainAudio").GetComponent<AudioSource> ();
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void OnTriggerEnter2D (Collider2D other)
@@ -23,10 +25,39 @@ public class LevelSwitch : MonoBehaviour
 
     IEnumerator NextLevel ()
     {
+        StartCoroutine(Chill());
         yield return new WaitForSeconds (MusicPlayer.levelWin.length);
         int currentIndex = SceneManager.GetActiveScene ().buildIndex;
         int nextIndex = currentIndex + 1;
         SceneManager.LoadScene (nextIndex);
         yield return null;
+    }
+
+    IEnumerator Chill()
+    {
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+        player.GetComponent<Player_controls>().disable = true;
+        yield return new WaitForEndOfFrame();
+        player.GetComponent<Animator>().SetBool("Walk", false);
+
+        yield return new WaitForSeconds(1.0f);
+
+        player.GetComponent<Animator>().SetBool("Walk", true);
+
+        float move = player.transform.position.y + 0.4f;
+        while (player.transform.position.y <= move)
+        {
+            Vector2 pos = player.transform.position;
+            pos.y += 0.1f;
+            pos.x += 0.1f;
+
+            player.transform.position = pos;
+            yield return new WaitForSeconds(0.1f);
+        }
+        player.GetComponent<Animator>().SetBool("Walk", false);
+
+        yield return new WaitForSeconds(0.7f);
+
+        player.GetComponent<Animator>().SetBool("Crouch", true);
     }
 }
